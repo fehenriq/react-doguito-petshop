1aa150425368184125dc8b57173b7ac27e75fb72
Felipe <lipe.h.r@gmail.com>
2022-09-30 10:45:46 -0300

    feito pagina 404 e criado navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import './assets/css/base/base.css'
-import Home from './pages/Home'
-import Sobre from './pages/Sobre'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-function App() {
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Pagina404 from './pages/Pagina404'
+import Cabecalho from './components/Cabecalho'
 
+function App() {
   return (
     <Router>
+      <Cabecalho />
       <Switch>
         <Route exact path='/'>
           <Home />
         </Route>
         <Route path='/sobre'>
           <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
         </Route>
       </Switch>
     </Router>
